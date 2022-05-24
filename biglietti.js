5bcb19d797a722ba8aa54a4d8415786055493565
Diakoretina <francesco.tammaro2000@gmail.com>
2022-05-24 19:02:18 +0200

    Add ceil

--- biglietti.js
+++ biglietti.js
@@ -1,18 +1,24 @@
-let kilom = prompt("Inserisci Kilometri");
-let età = prompt("Inserisci età");
+let kilom = parseInt(prompt("Inserisci Kilometri"));
+let età = parseInt(prompt("Inserisci età"));
 let sconto;
 let prezzo;
+
+if(isNaN(kilom) || isNaN(età)){
+    alert("Il valore inserito non è un numero. Riprova")
+}else{
+    alert("Prezzo del biglietto calcolato")
+}
 
 prezzo=kilom*0.21;
 
 if(età<18){
-    sconto=(prezzo*20)/100
-    prezzo=prezzo-sconto;
+    sconto = (prezzo*20)/100
+    prezzo = prezzo-sconto;
 }else if(età>=65){
-    sconto=(prezzo*40)/100
-    prezzo=prezzo-sconto;
+    sconto = (prezzo*40)/100
+    prezzo = prezzo-sconto;
 }
 
 document.getElementById(
     "base"
-  ).innerText = `Prezzo: ${prezzo}`;
+  ).innerText = `Prezzo: ${Math.ceil(prezzo)}`;
